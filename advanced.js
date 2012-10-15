63fa41d5e0a4ab90cdd5ad6c00226be03f01691e
Sune Simonsen <sune@we-knowhow.dk>
2012-10-15 20:54:07 +0200

    Using a stack layout on small screens

---
+++ advanced.js
@@ -0,0 +1,9 @@
+$(function () {
+    // Just some javascript to make the demo a little interactive
+    $('.item').click(function () {
+        $('.content').addClass('showMailView');
+    });
+    $('.back-to-inbox').click(function () {
+        $('.content').removeClass('showMailView');
+    });
+});

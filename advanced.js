a4c4fe2f8ee3698025d0a2376b82e9df0b35916a
Sune Simonsen <sune@we-knowhow.dk>
2012-12-19 17:55:29 +0100

    only transition on user interaction

--- advanced.js
+++ advanced.js
@@ -1,10 +1,21 @@
 $(function () {
     document.addEventListener("touchstart", function(){}, true);
+
+    function enableTransition() {
+        var views = $('.mailView, .mailList');
+        views.addClass('transitioning');
+        setTimeout(function () {
+            views.removeClass('transitioning');
+        }, 500);
+    }
     // Just some javascript to make the demo a little interactive
     $('.item').click(function () {
+        enableTransition();
         $('.content').addClass('showMailView');
     });
+
     $('.back-to-inbox').click(function () {
+        enableTransition();
         $('.content').removeClass('showMailView');
     });
 });
